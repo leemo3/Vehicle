22a832debd6ce467b28181276086969e0a433021
alnay <alnay@DESKTOP-HQTVOPH>
2022-02-20 18:42:28 -0500

    Vehicle test

---
+++ Vehicle/src/VehiclesTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class VehiclesTest {
+
+  @Test
+  public void testVehiclesStringBoolean() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetDrive() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testSetDrive() {
+    fail("Not yet implemented");
+  }
+
+}
